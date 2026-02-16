e53f5fb97fc9ddc57f6f3e4ce9e02e17c7437f2e
Prosenjit-dev-eng <hawladeprasenjit23@gmail.com>
2026-02-17 00:24:41 +0530

    Optimised

--- Clg/Assignment1/prime.py
+++ Clg/Assignment1/prime.py
@@ -2,7 +2,7 @@ limit = int(input("Generates primes up to: "))
 def primegen(limit):
     for num in range(2,limit + 1):
         isP = True
-        for i in range(2, num):
+        for i in range(2, int(num**0.5)+1):
             if num%i == 0:
                 isP = False
                 break
